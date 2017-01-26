a25854b2a9370d18cdc0b141136a219206e5fbc7
hungduong <viethungqb0609@gmail.com>
2017-01-25 20:19:08 -0600

    Add solution: 1. Combinations 2. Combinations Sum ||| 3. Gray Code 4. Permutation Sequence

---
+++ src/edu/mum/medium/Backtracking/CombinationSumIII.java
@@ -0,0 +1,7 @@
+package edu.mum.medium.Backtracking;
+
+/**
+ * Created by hungduong on 1/25/17.
+ */
+public class CombinationSumIII {
+}
---
+++ src/edu/mum/medium/Backtracking/Combinations.java
@@ -0,0 +1,39 @@
+package edu.mum.medium.Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hungduong on 1/25/17.
+ */
+public class Combinations {
+    //Use Backtracking
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        combineHelper(n, k, 1, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    public static void combineHelper(int n, int k, int i, List<Integer> list, List<List<Integer>> result) {
+        if(k == 0) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for(int j = i; j <= n; j++) {
+            list.add(j);
+            combineHelper(n, k - 1, j + 1, list, result);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = combine(4, 2);
+
+        for (List<Integer> ls : result) {
+            System.out.println(ls);
+        }
+    }
+}
